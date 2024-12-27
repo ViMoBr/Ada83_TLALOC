ef0fb89b352bddd2c5c867e72b36302945b228b3
VMo <vincent.morin@univ-brest.fr>
2024-12-27 18:21:35 +0100

    nettoyages

--- compile_gnat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-  set -x
-
-  cd $(dirname $0)/build
-
-  ADA_COMP_SRCS="-aI../src/ada_comp -aI../src/communs -aI../src/par_phase -aI../src/sem_phase -aI../src/code_gen -aI../src/pretty -I../bin/idl_tools"
-
-  gnatmake $ADA_COMP_SRCS -D ./ -v -g -gnat83  $2
