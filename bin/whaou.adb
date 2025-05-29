06164b6869d2ac1b86730abf07b33a541c0cea03
VMo <vincent.morin@univ-brest.fr>
2025-05-29 20:55:58 +0200

    wiki

---
+++ bin/whaou.adb
@@ -0,0 +1,10 @@
+			-----
+procedure			WHAOU
+is			-----
+  WHAOU	:constant WHAOU := WHAOU;
+  type WHAOU is range WHAOU .. WHAOU;
+  WHAOU	: WHAOU	:= WHAOU;
+begin
+  WHAOU( " WHAOU " );
+end	WHAOU;
+	-----
